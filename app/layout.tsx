621986a366c0e94b79779efb38bf5518c77555ff
majedalabdali2024 <majedalabdali@gmail.com>
2025-06-17 23:00:39 +0300

    Remove global footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
 import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
@@ -44,17 +43,6 @@ export default function RootLayout({
           <div className="relative flex flex-col min-h-screen">
             <Toaster position="top-center" />
             <div className="flex-grow">{children}</div>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                {/* <span className="text-default-600">Powered by</span> */}
-                {/* <p className="text-primary">HeroUI</p> */}
-              </Link>
-            </footer>
           </div>
         </Providers>
       </body>
