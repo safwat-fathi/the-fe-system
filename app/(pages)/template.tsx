36007fe90273f5a4fd1cdd6e9ff95ef75e01d601
Safwat Fathi <safwat.rashwan@gmail.com>
2025-11-15 09:59:48 +0200

    fix: show loading on page transition

---
+++ app/(pages)/template.tsx
@@ -0,0 +1,12 @@
+import { Suspense } from "react";
+
+import AppLoading from "@/components/AppLoading";
+
+export default function Template({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Suspense fallback={<AppLoading />}>{children}</Suspense>;
+}
+
